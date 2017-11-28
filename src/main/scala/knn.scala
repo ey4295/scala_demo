6efd16eb9d70ae1fccc1aae92912ea704c02055f
xuqh <865253988@qq.com>
2017-11-28 20:38:06 +0800

    object版本ＫＮＮ算法简单实现 数据源：ＩＲＩＳ

--- src/main/scala/knn.scala
+++ src/main/scala/knn.scala
@@ -1,103 +1,96 @@
 import scala.io.Source
 import scala.util.Random
+import math._
 
 /**
-  * knn
+  * knn_v2
   * Created by xuqh
-  * 2017/11/26
-  * 下午12:30
-  * description: implementation of k-nearest neighbour algorithm
+  * 2017/11/27
+  * 下午6:05
+  * description: implementation of knn
   */
 object knn {
 
-
-  def classify(data: List[Array[Float]], numOfFeats: Int, x: Array[Float], N: Int, CATEGORY: Map[String, String]): Int = {
-    /**
-      * param data: training data
-      * param numOfFeates: number of features
-      * param x: data to be classified
-      * param N: number of neighbours
-      * param CATEGORY: category reference table
-      * classify given x into category elected by its neighbours
+  def splitTrainTest(feats: Array[Array[Double]], labels: Array[String], testSize: Int): (Array[Array[Double]], Array[String], Array[Array[Double]], Array[String]) = {
+    /** *
+      * split train and test samples
+      * param feats: features
+      * param labels: feature labels
+      * param testSize: number of test samples
+      * return: 4 element tuple of (trainFeats,trainLabel,testFeats,testLabels)
       */
-    def dot(x: Array[Float], y: Array[Float]) = (0 to x.length - 1).map(i => x(i) * y(i)).sum
-
-    def vote(cate: Array[Float], cateRef: Map[String, String]): Int = {
-      /**
-        * vote for categories occurs most
-        * param cate: category list occurred
-        * param cateRef: category mapping table;used to get all possible catagories
-        */
-      var tmp1 = cateRef.values.map(labelRef => (labelRef.toInt, cate.count(label => label.toInt == labelRef.toInt)))
-      var tmp2 = tmp1.maxBy { case (label: Int, count: Int) => count }
-      var tmp3 = tmp2._1
-      tmp3
-
-    }
-
-    def quickSort(xs: List[(Float, Int)]): List[(Float, Int)] = {
-      /**
-        * list to be sorted ;[(value,index)]
-        */
-      if (xs.length <= 1) xs
-      else {
-        var pivot = xs(xs.length / 2)._1
-        quickSort(xs filter { t => t._1 < pivot }) ::: (xs filter { t => t._1 == pivot }) ::: quickSort(xs filter { t => t._1 > pivot })
-      }
-
-    }
-
-    // get neighbours
-    val dist = data.map(p => dot(p.slice(0, 3), x)).zipWithIndex
-    val sorted_dist = quickSort(dist)
-    val neighbours_index = sorted_dist.slice(0, N).map(p => p._2)
-    // vote
-    val neighboursCatagories = neighbours_index.map(index => data(index)(numOfFeats)).toArray
-    vote(neighboursCatagories, CATEGORY)
-
-
+    val indices = Seq.fill(testSize)(Random.nextInt(labels.length - 1))
+    val testFeats = (indices map { index => feats(index) }).toArray
+    val testLabels = (indices map { index => labels(index) }).toArray
+    val trainFeats = feats diff testFeats
+    val trainLabel = labels diff testLabels
+    (trainFeats, trainLabel, testFeats, testLabels)
   }
+
+  def distance(x: Array[Double], y: Array[Double]): Double = sqrt(((x zip y) map { case (xi, yi) => pow(yi - xi, 2) }).sum)
+
+
+  def quickSort(xs: Array[(Double, String)]): Array[(Double, String)] = {
+    if (xs.length <= 1) xs
+    else {
+      val pivot = xs(xs.length / 2)._1
+      Array.concat(quickSort(xs.filter(t => t._1 < pivot)), xs.filter(t => t._1 == pivot), quickSort(xs.filter(t => t._1 > pivot))
+      )
+    }
+  }
+
+  def classify(trainFeats: Array[Array[Double]], trainLabels: Array[String], numOfFeats: Int, x: Array[Double], N: Int): String = {
+    /**
+      * classify a single sample by voting
+      * param trainFeats:training samples features
+      * param trainLabels:training samples labels
+      * param numOfFeats: number of features
+      * param x:sample to be classified
+      * param N:number of neighbours which have voting right
+      * return:category name (label)
+      */
+
+    // calculate distances
+    val dists = trainFeats map { feat => distance(feat, x) }
+    // get neighbours
+    val neighboursLabels = quickSort(dists zip trainLabels).slice(0, N - 1).map(x => x._2)
+    // vote
+    val labelNames = neighboursLabels.distinct
+    val labelCounts = labelNames.map(labelName => (labelName, neighboursLabels.count(label => label == labelName)))
+    labelCounts.maxBy({ case (_, count) => count })._1
+  }
+
+  def eval(predictedLabels: Array[String], acutalLabels: Array[String]): Int = (predictedLabels zip acutalLabels).count(t => t._1 == t._2) / acutalLabels.length
 
   def main(args: Array[String]): Unit = {
     //fetch data
     //clean data
     val numOfFeats = 4
     val testSize = 15
-    val CATEGORY = Map("Iris-virginica" -> "0", "Iris-versicolor" -> "1", "Iris-setosa" -> "2")
+    val N = 40
+    // read data
     val input = Source.fromFile("dataset/iris.data").getLines()
-      .map(line => {
-        val array = line.split(",")
-        array(numOfFeats) = CATEGORY(array(numOfFeats))
-        array.map(p => p.toFloat)
-      })
-      .toArray
-    //    var minAndMax=((0 to numOfFeats-1) map {col=>(rawInput.map(p=>p(col)).min,rawInput.map(p=>p(col)).max)}).toArray
-    //todo normalization
-//    val input = Array.concat(
-//      (0 to numOfFeats - 1).map({
-//        row =>
-//          val rawInputT = rawInput.transpose
-//          rawInputT(row).map(x => x / (rawInputT(row).max - rawInputT(row).min))
-//      }).toArray
-//      , {
-//        val rawInputT = rawInput.transpose
-//        rawInputT(numOfFeats)
-//      }).transpose.toList
-    //split data into test and training
-    val testSampleIndex = Seq.fill(testSize)(Random.nextInt(input.length))
-    val trainSampleIndex = (0 to input.length - 1) diff testSampleIndex
-    val testSample = testSampleIndex.map(index => input(index)).toList
-    val trainSample = trainSampleIndex.map(index => input(index)).toList
-
-    //classify 1.calculate distance 2.quick sort 3 generate result
-    for (sample <- testSample) {
-      var myres = classify(trainSample, numOfFeats, sample.slice(0, 3), 3, CATEGORY)
-      var trueRes = sample(4)
-      println(s"my=$myres,true=$trueRes")
-
-    }
-    //todo calculate accuracy
+      .map(
+        line => {
+          val arr = line.split(",")
+          (arr.slice(0, numOfFeats), arr(numOfFeats))
+        }).toArray
+    val featsRaw = input map { p => p._1 }
+    val labels = input map { p => p._2 }
+    // normalization
+    val featsRawT = featsRaw.transpose
+    val feats = ((0 until numOfFeats) map {
+      row => {
+        val featT = featsRawT(row).map(x => x.toDouble)
+        featT map { x => (x - featT.min) / (featT.max - featT.min) }
+      }
+    }).toArray.transpose
+    val (trainFeats, trainLabel, testFeats, testLabels) = splitTrainTest(feats, labels, testSize)
+    // classification
+    val predictedLabels = testFeats map { feat => classify(trainFeats, trainLabel, numOfFeats, feat, N) }
+    // evaluation
+    val accuracy = eval(predictedLabels, testLabels)
+    for ((pred, actual) <- predictedLabels.zip(testLabels)) println(s"predicted=$pred,   actual=$actual")
+    println(s"Accuracy=$accuracy")
   }
 }
-
-
